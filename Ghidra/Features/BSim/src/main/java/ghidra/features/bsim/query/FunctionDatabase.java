2697fe7ca9378ff79ced24526b404586e245bcb2
Joe Mansour <173751+jsmnsr@users.noreply.github.com>
2025-06-27 22:25:54 -0400

    Add XXE test and refine secure parsing

--- Ghidra/Features/BSim/src/main/java/ghidra/features/bsim/query/FunctionDatabase.java
+++ Ghidra/Features/BSim/src/main/java/ghidra/features/bsim/query/FunctionDatabase.java
@@ -21,6 +21,8 @@ import java.util.*;
 import javax.xml.parsers.*;
 import javax.xml.XMLConstants;
 
+import ghidra.util.xml.XmlUtilities;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
@@ -385,11 +387,13 @@ public interface FunctionDatabase extends AutoCloseable {
 
 		try {
                        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                       factory.setFeature(javax.xml.XMLConstants.FEATURE_SECURE_PROCESSING, true);
-                       factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-                       factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
-                       factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+                       factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+                       factory.setFeature(XmlUtilities.FEATURE_DISALLOW_DTD, true);
+                       factory.setFeature(XmlUtilities.FEATURE_EXTERNAL_GENERAL_ENTITIES, false);
+                       factory.setFeature(XmlUtilities.FEATURE_EXTERNAL_PARAMETER_ENTITIES, false);
                        factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+                       factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+                       factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
                        factory.setXIncludeAware(false);
                        factory.setExpandEntityReferences(false);
                        DocumentBuilder builder = factory.newDocumentBuilder();
